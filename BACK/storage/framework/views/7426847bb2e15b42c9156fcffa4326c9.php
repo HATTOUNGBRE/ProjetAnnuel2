157249c1714a909efcc28819ba9e33b73acfc290
HATTOUNGBRE <attoungbreha@hotmail.com>
2024-04-07 18:42:47 +0200

    ajout des branches

---
+++ BACK/storage/framework/views/7426847bb2e15b42c9156fcffa4326c9.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/hallyaattoungbre/Document/COURS 2023/S2/Projet Annuel/Back_PA/vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php ENDPATH**/ ?>
