4d729d1c4e841693ccb354e5403ea6d3358f04e3
Hallya ATTOUNGBRE <hallyaattoungbre@MacBook-Air-de-Hallya.local>
2024-04-19 13:41:17 +0200

    Projet Front Header et Routes

---
+++ FRONT/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function Main() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        Coucou
+    </div>
+    );
+}
+
+    export default Main;
