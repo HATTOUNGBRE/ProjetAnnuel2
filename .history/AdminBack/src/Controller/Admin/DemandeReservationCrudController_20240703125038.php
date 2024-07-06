8c4036ce65a166bdc8abdc4a68b3e6b3c43db29a
Hallya ATTOUNGBRE <hallyaattoungbre@MacBook-Air-de-Hallya.local>
2024-07-06 16:27:28 +0200

    ajout de la verif inscription + envoi d'un mail + recherche nb de voyageur

---
+++ .history/AdminBack/src/Controller/Admin/DemandeReservationCrudController_20240703125038.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\DemandeReservation;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+
+
+class DemandeReservationCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return DemandeReservation::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IntegerField::new('id')->hideOnForm(),
+            TextField::new('reservationNumber', 'Numéro de réservation'),
+            DateField::new('dateArrivee'),
+            DateField::new('dateDepart'),
+            IntegerField::new('guestNb'),
+            AssociationField::new('property'),
+            TextField::new('status'),
+            TextField::new('name')->hideOnForm(),
+            TextField::new('surname')->hideOnForm(),
+            IntegerField::new('voyageurId')->hideOnForm(),
+            MoneyField::new('totalPrice')->setCurrency('EUR')->setStoredAsCents(false),
+            DateTimeField::new('createdAt')->hideOnIndex(),
+            DateTimeField::new('updatedAt')->hideOnIndex(),
+            BooleanField::new('active'),
+        ];
+    }
+
+    #[Route('/api/demandes/active', name: 'get_active_demandes', methods: ['GET'])]
+    public function getActiveDemandes(): JsonResponse
+    {
+        $demandes = $this->demandeRepository->findBy(['active' => true]);
+
+        if (!$demandes) {
+            return new JsonResponse([], JsonResponse::HTTP_OK);
+        }
+
+        return $this->json($demandes, 200, [], ['groups' => 'demande:read']);
+    }
+}
---
+++ .history/AdminBack/src/Controller/ReservationVoyageurController_20240703170825.php
@@ -0,0 +1,72 @@
+<?php
+// src/Controller/ReservationVoyageurController.php
+
+namespace App\Controller;
+
+use App\Entity\ReservationVoyageur;
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use App\Repository\ReservationVoyageurRepository;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ReservationVoyageurController extends AbstractController
+{
+    private $entityManager;
+    private $reservationVoyageurRepository;
+    private $serializer;
+
+    public function __construct(
+        EntityManagerInterface $entityManager, 
+        ReservationVoyageurRepository $reservationVoyageurRepository, 
+        SerializerInterface $serializer
+    ) {
+        $this->entityManager = $entityManager;
+        $this->reservationVoyageurRepository = $reservationVoyageurRepository;
+        $this->serializer = $serializer;
+    }
+
+    #[Route('/api/reservations', name: 'create_reservation', methods: ['POST'])]
+    public function createReservation(Request $request, PropertyRepository $propertyRepository, ValidatorInterface $validator): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $propertyId = $data['property'] ?? null;
+        $property = $propertyRepository->find($propertyId);
+
+        if (!$property) {
+            return new JsonResponse(['message' => 'Property not found'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        $reservation = new ReservationVoyageur();
+        $reservation->setDateArrivee(new \DateTime($data['dateArrivee']));
+        $reservation->setDateDepart(new \DateTime($data['dateDepart']));
+        $reservation->setGuestNb($data['guestNb']);
+        $reservation->setProperty($property);
+
+        $errors = $validator->validate($reservation);
+
+        if (count($errors) > 0) {
+            $errorsString = (string) $errors;
+            return new JsonResponse(['message' => $errorsString], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        $this->entityManager->persist($reservation);
+        $this->entityManager->flush();
+
+        return new JsonResponse(['message' => 'Reservation created successfully'], JsonResponse::HTTP_CREATED);
+    }
+
+    #[Route('/api/properties/{id}/reservations', name: 'get_property_reservations', methods: ['GET'])]
+    public function getPropertyReservations(int $id): JsonResponse
+    {
+        $reservations = $this->reservationVoyageurRepository->findBy(['property' => $id]);
+
+        $responseData = $this->serializer->serialize($reservations, 'json', ['groups' => 'reservation:read']);
+        return new JsonResponse($responseData, JsonResponse::HTTP_OK, [], true);
+    }
+}
